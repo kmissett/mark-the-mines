a83eb7d220bb0ab63ee2f2c0e84d2d03fc296806
kmissett <86633265+kmissett@users.noreply.github.com>
2022-04-07 20:32:44 -0400

    Added localStorage support for preserving current level, and refactored to perform adjacency calculations on click in order to speed up loading.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mark-the-mines
+
+A vanilla JS version of Minesweeper. It was a little slow to load at the highest difficulty level, so I refactored things so that the adjacency calculations happen on the fly instead of all at build time. I also added storing the current difficulty level in localStorage, so that when you come back to (or reload) the page, it doesn't just default to the beginner level every time.
+
+TODO:
+1. Adding a timer / best time feature, to be kept in localStorage too?
+2. Keep track of the number of games won at each difficulty level / average percentages?
+3. Maybe a more dynamic difficulty level chooser than just the three?
+4. Accessibility stuff
--- script.js
+++ script.js
@@ -1,16 +1,51 @@
 const modal = document.getElementById("modal-overlay")
+const gameBoard = document.querySelector(".game-board")
+const markersRemaining = document.querySelector(".remaining span")
 
-const GameBoard = (numMines = 10, numRows = 10, numCols = 10) => {
-  
+const GameBoard = (level) => {
+  let numMines, numRows, numCols
+  switch(level) {
+    case(100):
+      numMines = 100
+      numRows = 25
+      numCols = 40
+      break
+    case(40):
+      numMines = 40
+      numRows = 20
+      numCols = 20
+      break
+    case(10):
+      numMines = 10
+      numRows = 10
+      numCols = 10
+      break
+    default:
+      break
+  }
+
   const numCells = numRows * numCols
+  markersRemaining.textContent = numMines
+
+  // generate mine locations
   let mineLocations = []
   while (mineLocations.length < numMines) {
     const location = Math.floor(Math.random() * numCells)
     if (!mineLocations.includes(location)) mineLocations.push(location)
   }
   mineLocations.sort((a,b) => a - b)
+  
+  
 
-  const adjacentCellIndices = (index) => {
+  const drawCells = () => {
+    let cells = ""
+    for(let i=0; i < numRows * numCols; i++) { cells += `<button data-index="${i}" class="cell"></button>` }
+    return cells
+  }
+
+  gameBoard.innerHTML = drawCells()
+
+  const adjacentIndices = (index) => {
     const row = Math.floor(index/numCols)
     const col = index % numCols
     let isAdjacent = []
@@ -30,167 +65,124 @@ const GameBoard = (numMines = 10, numRows = 10, numCols = 10) => {
     return isAdjacent.sort((a, b) => a - b)
   }
 
-  let cells = []
-  for(let i=0; i< numCells; i++) {
-    const newCell = {
-      "index": i, 
-      "row": Math.floor(i/numCols), 
-      "col": i % numCols, 
-      "hasMine": mineLocations.includes(i),
-      "adjacent": adjacentCellIndices(i),
-      "revealed": false,
-      "flagged": false
-    }
-    cells.push(newCell)
-    cells.forEach(cell => {
-      cell.adjacentMineCount = cell.adjacent.filter(item => mineLocations.includes(item)).length
-    })
-  }
-  
-  const drawCells = () => {
-    cells.forEach(cell => gameBoard.innerHTML += `<button class="cell" data-index="${cell.index}"></button>`)
-  }
-
-  let flaggedLocations = []
-  document.querySelector(".remaining span").textContent = numMines - flaggedLocations.length
-  
-  const toggleFlag = (currentIndex) => {
-    const cell = cells[currentIndex]
-    if (cell.revealed) return
+  let revealedLocations = []
+  const showCell = (currentIndex) => {
+    const index = parseInt(currentIndex)
+    const currentCell = document.querySelector(`[data-index="${index}"]`)
     
-    const cellButton = document.querySelector(`.cell[data-index="${currentIndex}"]`)
-    if (cell.flagged) {
-      cellButton.classList.remove("flagged")
-      flaggedLocations.splice(flaggedLocations.indexOf(currentIndex),1)
-      cell.flagged = !cell.flagged
+    if (currentCell.hasAttribute("data-revealed") || currentCell.hasAttribute("data-marked")) return
+    
+    currentCell.setAttribute("data-revealed", "")
+    revealedLocations.push(index)
+
+    if (mineLocations.includes(index)) {
+      loseGame()
+      return
     }
     else {
-      if(flaggedLocations.length === numMines) return
-      cellButton.classList.add("flagged")
-      flaggedLocations.push(currentIndex)
-      flaggedLocations.sort((a,b) => a-b)
-      cell.flagged = !cell.flagged
+      const adjacent = adjacentIndices(index)
+      const adjacentMines = adjacent.filter(index => mineLocations.includes(index))
+      const adjacentMineCount = adjacentMines.length
+      if (adjacentMineCount > 0) currentCell.textContent = adjacentMineCount
+      else {adjacent.forEach(index=>showCell(index))}
+    }
+    checkWin()
+  }
+  
+  let markedLocations = []
+  const toggleMark = (e) => {
+    const currentCell = e.target
+    const currentIndex = currentCell.dataset.index
+    
+    if (currentCell.hasAttribute("data-revealed")) return
+
+    if(currentCell.hasAttribute("data-marked")) {
+      currentCell.removeAttribute("data-marked")
+      markedLocations = markedLocations.filter(item => item !== currentIndex)
+    }
+    else {
+      if(markedLocations.length === numMines) return
+      currentCell.dataset.marked = true
+      markedLocations.push(currentIndex)
+      markedLocations.sort((a,b) => a-b)
     }
 
-    document.querySelector(".remaining span").textContent = numMines - flaggedLocations.length
+    markersRemaining.textContent = numMines - markedLocations.length
     checkWin()
-    // console.log(flaggedLocations)
   }
 
-  let revealedLocations = []
-  const showContent = (currentIndex) => {
-    const cell = cells[currentIndex]
-    
-    // cell already revealed, or cell was flagged -> do nothing
-    if (cell.revealed || cell.flagged) return
-    
-    cell.revealed = true
-    revealedLocations.push(currentIndex)
-    
-    const cellButton = document.querySelector(`.cell[data-index="${currentIndex}"]`)
-    cellButton.classList.add("revealed")
-
-    // cell has a mine -> lose game
-    if (cell.hasMine) {
-      cellButton.textContent = "*"
-      cellButton.style.backgroundColor = "pink"
-      loseGame()
+  const handleClick = (e) => {
+    const currentIndex = parseInt(e.target.dataset.index)
+    if (e.button === 0) showCell(currentIndex)
+    if (e.button === 1) {
+      e.preventDefault()
+      const adjacent = adjacentIndices(currentIndex)
+      adjacent.forEach(index => showCell(index))
     }
-
-    else if (cell.adjacentMineCount > 0) cellButton.textContent = cell.adjacentMineCount
-    else {cell.adjacent.forEach(adj => showContent(adj))}
-
-    checkWin()
   }
 
   const loseGame = () => {
     showModal("lose")
     gameBoard.style.pointerEvents = "none"
-
+    
     document.querySelectorAll(".cell").forEach(item => {
-      const currentCell = cells[item.dataset.index]
-      currentCell.revealed = true
-      item.classList.add("revealed")
-      if (currentCell.hasMine) {
+      item.dataset.revealed = true
+      const currentIndex = parseInt(item.dataset.index)  
+      if (mineLocations.includes(currentIndex)) {
         item.textContent = "*"
-        if (!currentCell.flagged) item.style.backgroundColor = "var(--color-mine-light)"
+        if (!item.hasAttribute("data-marked")) item.style.backgroundColor = "var(--color-mine-light)"
       }
-      else item.textContent = (currentCell.adjacentMineCount > 0) ? currentCell.adjacentMineCount : ""
+      else {
+        const adjacent = adjacentIndices(currentIndex)
+        const adjacentMineCount = adjacent.filter(index => mineLocations.includes(index)).length
+        item.textContent = (adjacentMineCount > 0) ? adjacentMineCount : ""
+      }
     })
   }
 
   const checkWin = () => {
-    if (revealedLocations.length + flaggedLocations.length === numCells) {
+    if (markedLocations.length < numMines) return
+    if (revealedLocations.length === numCells - numMines) {
+      if (revealedLocations.some(location => mineLocations.includes(location))) {
+        loseGame()
+        return
+      }
       showModal("win")
       gameBoard.style.pointerEvents = "none"
     }
   }
 
-  return {
-    mines: numMines,
-    rows: numRows,
-    cols: numCols,
-    get numCells() {return this.rows * this.cols},
-    get mineLocations() {return mineLocations},
-    get cells() {return cells},
-    get drawCells() {return drawCells()},
-    toggleFlag: function(i) {return toggleFlag(i)},
-    showContent: function(i) {return showContent(i)}
-  }
+  return { toggleMark, handleClick }
 }
 
-const startGame = (numMines = 10, numRows = 10, numCols = 10) => {
- 
-  
-
-  const gameBoard = document.getElementById("gameBoard")
+const startGame = () => {
   gameBoard.innerHTML = ""
-  gameBoard.dataset.level = numMines
   gameBoard.style.pointerEvents = "auto"
-  
-  const game = GameBoard(numMines, numRows, numCols)
-  game.drawCells
+  let currentLevel = window.localStorage.getItem("currentLevel") || 10
+  gameBoard.dataset.level = currentLevel
+  const level = parseInt(currentLevel)
+  const game = GameBoard(level)
 
-
-  // add event listeners to the gameboard
-  document.querySelectorAll(".cell").forEach(button => {
-    button.addEventListener("contextmenu", (e) => {
-      e.preventDefault()
-      game.toggleFlag(e.target.dataset.index)
-    })
-    button.addEventListener("mousedown", (e) => {
-      const currentIndex = e.target.dataset.index
-      if(e.button === 0) game.showContent(currentIndex)
-      if(e.button === 1) {
-        e.preventDefault()
-        game.cells[currentIndex].adjacent.forEach(index=> game.showContent(index))
-        if (!e.target.classList.contains("revealed")) game.showContent(currentIndex)
-      }
-
-    })
-
-      // debug mode
-      button.addEventListener("mouseover", (e) => { if (game.cells[e.target.dataset.index].hasMine) debug.style.backgroundColor = "var(--color-mine)"})
-      button.addEventListener("mouseout", () => { debug.style.backgroundColor = "var(--color-light)"})
-  })
+  const cellButtons = document.querySelectorAll(".cell")
+  cellButtons.forEach(button=> button.addEventListener("mousedown", (e) => game.handleClick(e)))
+  cellButtons.forEach(button=> button.addEventListener("contextmenu", (e) => {
+    e.preventDefault()
+    game.toggleMark(e)
+  }))
 }
+
 
 startGame()
 
 // restart the game at the current level
-document.getElementById("reset").addEventListener("click", () => {
-  if (gameBoard.dataset.level === "100") startGame(100,25,40)
-  else if (gameBoard.dataset.level === "40") startGame(40,20,20)
-  else startGame()
-})
+document.getElementById("reset").addEventListener("click", () => startGame)
 
 // change the level and restart the game
-document.querySelectorAll(".level-button").forEach(levelButton => levelButton.addEventListener("click", () => {
-  if(levelButton.dataset.level === "100") startGame(100,25,40)
-  else if(levelButton.dataset.level === "40") startGame(40,20,20)
-  else startGame()
+document.querySelectorAll(".level-button").forEach(levelButton => levelButton.addEventListener("click", (e) => {
+  const level = parseInt(e.target.dataset.level)
+  window.localStorage.setItem("currentLevel", level)
+  startGame()
 }))
-
 
 const showModal = (message) => {
   const messages = {
@@ -209,4 +201,6 @@ const showModal = (message) => {
 }
 document.querySelector(".info-button").addEventListener("click", () => showModal("info"))
 document.querySelector(".close-button").addEventListener("click", () => modal.classList.remove("active"))
-
+window.addEventListener("keydown", (e) => {
+  if(e.key === "Escape" && modal.classList.contains("active")) modal.classList.remove("active")
+})
